6b0ecf82080acf255416043e49fee59694471f5b
Anshul <kh.anshul04@gmail.com>
2025-06-17 23:39:40 +0530

    init

---
+++ cmd/daemon/daemon.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+
+	dm "github.com/sevlyar/go-daemon"
+)
+
+var (
+	logDir      = "/var/log/zep"
+	pidDir      = "/var/run/zep"
+	logFilePath = "/var/log/zep/zep.log"
+	pidFilePath = "/var/run/zep/zep.pid"
+)
+
+const (
+	START   = "start"
+	STOP    = "stop"
+	RESTART = "restart"
+	STATUS  = "status"
+)
+
+func setupFiles() {
+	err := os.MkdirAll(pidDir, 0755)
+	if err != nil {
+		log.Fatalf("failed to pid directory:%v", err)
+	}
+
+	err = os.MkdirAll(logDir, 0755)
+	if err != nil {
+		log.Fatalf("failed to create log dir:%v", err)
+	}
+}
+
+func startDaemon() {
+	setupFiles()
+
+	cntx := &dm.Context{
+		LogFileName: logFilePath,
+		LogFilePerm: 0664,
+		PidFileName: pidFilePath,
+		PidFilePerm: 0664,
+		Umask:       027,
+		WorkDir:     "/",
+	}
+
+	d, err := cntx.Reborn()
+	if err != nil {
+		log.Printf("failed to create daemon:%v", err)
+	}
+
+	if d != nil {
+		return
+	}
+
+	defer cntx.Release()
+
+	log.Printf("daemon started with pid:%d", os.Getpid())
+
+}
+
+func stopDaemon() {
+	d := &dm.Context{
+		PidFileName: pidFilePath,
+	}
+
+	p, err := d.Search()
+
+	if err != nil || p == nil {
+		log.Fatalf("failed to stop daemon,(may be not running)")
+	}
+
+	err = p.Signal(syscall.SIGTERM)
+	if err != nil {
+		log.Fatalf("failed to stop daemon: %v", err)
+	}
+
+	log.Print("daemon stopped")
+}
+
+func restartDaemon() {
+	stopDaemon()
+	startDaemon()
+}
+
+func statusOfDaemon() {
+	d := &dm.Context{
+		PidFileName: pidFilePath,
+	}
+
+	p, err := d.Search()
+
+	if err != nil {
+		log.Fatalf("daemon not found,(may be not running)")
+	}
+
+	log.Printf("daemon running..., pid:%d", p.Pid)
+}
+
+func main() {
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("usage: zep start|stop|restart|status\n")
+		return
+	}
+
+	switch args[1] {
+	case START:
+		startDaemon()
+		return
+	case STOP:
+		stopDaemon()
+		return
+	case RESTART:
+		restartDaemon()
+		return
+	case STATUS:
+		statusOfDaemon()
+		return
+	default:
+		fmt.Printf("usage: zep start|stop|restart|status")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/anshul-kh/zep-core
+
+go 1.24.4
+
+require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/sevlyar/go-daemon v0.1.6 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
